7300436cb73ccc1bf15e5a3b706f2e7e6c5b43f2
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-07 22:47:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ end
 def monopoly_with_third_tier
 		monopoly = {
     :railroads => {:pieces => 4, :names =>{}, :rent_in_dollars => {
-      :pieces => 4
+      :one_piece_owned => 4
       
     }
   }
