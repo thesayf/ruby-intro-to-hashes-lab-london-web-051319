b971e6c358139060843f16c00ca9444849501b1d
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-07 22:45:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,11 +20,10 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-    :railroads => {
-      
-      
-    }
+    :railroads => {:pieces => 4, :names =>{}, :rent_in_dollars => {}
   }
+
+   }
 end
 
 def monopoly_with_third_tier
